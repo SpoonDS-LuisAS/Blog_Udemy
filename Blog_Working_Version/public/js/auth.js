0b7528c0b9a9fbb9c3b1cb7780f7d7b100346024
luisarendsanchez <luis.arend@spoonds.com>
2020-03-02 20:40:38 +0100

    Adding Folders

---
+++ Blog_Working_Version/public/js/auth.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        user_login:{
+            csrf: $('input[name=_csrf]').val(),
+            username:'',
+            password:''
+        },
+        login_is_submitting: false,
+        login_show_alert: false,
+        login_alert_class: '',
+        login_alert_msg: ''
+    },
+    methods:{
+        login: function(){
+            this.login_is_submitting = true;
+            this.login_show_alert = true;
+            this.login_alert_class = 'infomsg';
+            this.login_alert_msg = 'please wait!';
+
+            $.post('/login', this.user_login).then((response)=>{
+                if(response.status === 2){
+                    this.login_alert_class = 'successmsg';
+                    this.login_alert_msg = 'Success! You are now being redirected.';
+                    location.href = '/';
+                }else{
+                    this.login_is_submitting = false;
+                    this.login_alert_class = 'errormsg';
+                    this.login_alert_msg = 'Invalid login info!';
+                }
+            });
+
+        }
+    }
+})
