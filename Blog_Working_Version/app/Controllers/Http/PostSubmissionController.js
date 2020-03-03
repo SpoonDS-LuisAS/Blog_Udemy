ec9cf60f6a7cf6925c55da694f3249223f655d45
luisarendsanchez <luis.arend@spoonds.com>
2020-03-03 16:17:44 +0100

    uploading a file

--- Blog_Working_Version/app/Controllers/Http/PostSubmissionController.js
+++ Blog_Working_Version/app/Controllers/Http/PostSubmissionController.js
@@ -1,8 +1,18 @@
 'use strict'
 
+const {validate} = use("Validator");
+
 class PostSubmissionController {
-    async submit(){
-        return "hello world";
+    async submit({request}){
+        let output = {status: 1};
+        const post_validation = await validate(request.all(), {
+            title:'required|min:3|max:255|string',
+            category:'required',
+            content:'required|string'
+        });
+        if(post_validation.fails()){
+            return output;
+        }
     }
 }
 
--- Blog_Working_Version/app/Models/Post.js
+++ Blog_Working_Version/app/Models/Post.js
@@ -4,6 +4,9 @@
 const Model = use('Model')
 
 class Post extends Model {
+    static get table(){
+        return 'posts';
+    }
 }
 
 module.exports = Post
